81ee4142a75e8311c3f0927e8b47524fdf55725f
Yves Desjardins <YvesDesjardins@gmail.com>
2019-04-05 10:40:42 -0700

    starter code

--- index.js
+++ index.js
@@ -36,11 +36,25 @@ app.post('/start', (request, response) => {
 // Handle POST request to '/move'
 app.post('/move', (request, response) => {
   // NOTE: Do something here to generate your move
+  const board = request.body.board;
+  const {
+    height,
+    width
+  } = board;
+  const myBody = request.body.you.body;
+  const data = {};
 
-  // Response data
-  const data = {
-    move: 'up', // one of: ['up','down','left','right']
+  // go in circles
+  data.move = avoidWall(myBody, height, width);
+  const food = eatFood(myBody, board);
+  const self = avoidSelf(board);
+  if (food) {
+    data.move = food;
   }
+  if (self) {
+    data.move = self;
+  }
+
 
   return response.json(data)
 })
@@ -54,6 +68,64 @@ app.post('/ping', (request, response) => {
   // Used for checking if this snake is still alive.
   return response.json({});
 })
+
+function avoidWall(myBody, height, width) {
+  switch (myBody[0].x) {
+    case 0:
+      if (myBody[0].y === 0)
+        return 'right';
+      else
+        return 'up';
+      break;
+    case width - 1:
+      if (myBody[0].y === height - 1)
+        return 'left';
+      else
+        return 'down';
+      break;
+    default:
+      if (myBody[0].y === 0)
+        return 'right';
+      else return 'left';
+      break;
+  }
+}
+
+function eatFood(myBody, board) {
+  const nearestFood = foodNearMe(myBody, board.food);
+
+  if (nearestFood) {
+    switch (nearestFood) {
+      case x > myBody[0].x:
+        return 'right';
+      case x < myBody[0].x:
+        return 'left';
+      case y > myBody[0].y:
+        return 'down';
+      case y < myBody[0].y:
+        return 'up';
+    }
+  }
+  return '';
+}
+
+function foodNearMe(myBody, foods) {
+  foods.forEach(food => {
+    if (Math.abs(myBody.x - food.x) <= 1) {
+      if (y = Math.abs(myBody.y - food.y) <= 1) {
+        return {
+          x: food.x,
+          y: food.y,
+        };
+      }
+    }
+  });
+}
+
+function avoidSelf(board) {
+
+  return '';
+}
 
 // --- SNAKE LOGIC GOES ABOVE THIS LINE ---
 
